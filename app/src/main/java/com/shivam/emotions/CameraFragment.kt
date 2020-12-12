a1d5ad01e1ea7c8292b5d0949447475f8e28460b
Shivam Sood <shivam.sood708@gmail.com>
2020-12-12 16:22:13 +0530

    Added camera emotion detect

--- app/src/main/java/com/shivam/emotions/CameraFragment.kt
+++ app/src/main/java/com/shivam/emotions/CameraFragment.kt
@@ -17,14 +17,32 @@ import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.otaliastudios.cameraview.controls.Facing
 import com.otaliastudios.cameraview.frame.Frame
 import com.shivam.emotions.databinding.FragmentCameraBinding
+import com.shivam.emotions.tfmodels.arousal.ArousalNetClassifier
+import com.shivam.emotions.tfmodels.arousal.ArousalNetConfig
+import com.shivam.emotions.tfmodels.arousal.ClassificationArousal
+import com.shivam.emotions.tfmodels.emotion.ClassificationEmotion
+import com.shivam.emotions.tfmodels.emotion.PrajuNetClassifier
+import com.shivam.emotions.tfmodels.emotion.PrajuNetConfig
+import com.shivam.emotions.tfmodels.valence.ClassificationValence
+import com.shivam.emotions.tfmodels.valence.ValenceNetClassifier
+import com.shivam.emotions.tfmodels.valence.ValenceNetConfig
+import com.shivam.emotions.util.EmotionConstants
+import com.shivam.emotions.util.EmotionUtil
 import com.shivam.emotions.util.PermissionsUtilCallback
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
 class CameraFragment : BaseFragment() {
     private lateinit var cameraBinding: FragmentCameraBinding
     private lateinit var detector: FaceDetector
+    private lateinit var prajuNetClassifier: PrajuNetClassifier
+    private lateinit var arousalNetClassifier: ArousalNetClassifier
+    private lateinit var valenceNetClassifier: ValenceNetClassifier
 
 
     private var rotationIMG = 270f
@@ -92,7 +110,9 @@ class CameraFragment : BaseFragment() {
             .build()
 
         detector = FaceDetection.getClient(options)
-
+        loadPrajuNetClassifier()
+        loadArousalNetClassifier()
+        loadValenceNetClassifier()
 
         val noFaceSnackBar = Snackbar.make(
             cameraBinding.root,
@@ -118,6 +138,46 @@ class CameraFragment : BaseFragment() {
                 }
             })
     }
+
+    private fun loadPrajuNetClassifier() {
+        try {
+            prajuNetClassifier =
+                PrajuNetClassifier.classifier(
+                    requireActivity().assets,
+                    PrajuNetConfig.MODEL_FILENAME
+                )
+        } catch (e: IOException) {
+            Timber.e("PrajuNet model couldn't be loaded. Check logs for details.")
+            e.printStackTrace()
+        }
+    }
+
+    private fun loadArousalNetClassifier() {
+        try {
+            arousalNetClassifier =
+                ArousalNetClassifier.classifier(
+                    requireActivity().assets,
+                    ArousalNetConfig.MODEL_FILENAME
+                )
+        } catch (e: IOException) {
+            Timber.e("ArousalNet model couldn't be loaded. Check logs for details.")
+            e.printStackTrace()
+        }
+    }
+
+    private fun loadValenceNetClassifier() {
+        try {
+            valenceNetClassifier =
+                ValenceNetClassifier.classifier(
+                    requireActivity().assets,
+                    ValenceNetConfig.MODEL_FILENAME
+                )
+        } catch (e: IOException) {
+            Timber.e("ValenceNet model couldn't be loaded. Check logs for details.")
+            e.printStackTrace()
+        }
+    }
+
 
     private fun visionImg(frame: Frame, faceCallback: FaceCallback) {
 
@@ -167,7 +227,103 @@ class CameraFragment : BaseFragment() {
                             faces[0].boundingBox.width()
                         )
 
-                        cameraBinding.ivFace.load(faceBitmap)
+
+
+                        GlobalScope.launch(Dispatchers.IO) {
+                            val finalBitmap = bitmapUtils.preparePrajuNetBitmap(faceBitmap)
+
+                            val emotions: List<ClassificationEmotion> =
+                                prajuNetClassifier.recognizeImage(finalBitmap)
+                            val arousals: List<ClassificationArousal> =
+                                arousalNetClassifier.recognizeImage(finalBitmap)
+                            val valences: List<ClassificationValence> =
+                                valenceNetClassifier.recognizeImage(finalBitmap)
+
+                            Timber.d("HHH $emotions \n$arousals \n$valences")
+
+                            try {
+
+                                val cEmotion = EmotionUtil.compoundEmotion(
+                                    emotions[0],
+                                    arousals[0],
+                                    valences[0]
+                                )
+
+                                Timber.d("JJJ $cEmotion")
+
+                                launch(Dispatchers.Main) {
+                                    val emotion = emotions[0].title
+                                    cameraBinding.tvEmotion.text = emotions[0].toString()
+                                    cameraBinding.tvCompoundEmotion.text = cEmotion
+                                    cameraBinding.ivFace.load(faceBitmap)
+
+                                    val attention = if (valences[0].title == 0) {
+                                        "Attention Level: ${
+                                            String.format(
+                                                "(%.1f%%) ",
+                                                (1 - valences[0].confidence) * 100.0f
+                                            )
+                                        }"
+                                    } else {
+                                        "Attention Level: ${
+                                            String.format(
+                                                "(%.1f%%) ",
+                                                valences[0].confidence * 100.0f
+                                            )
+                                        }"
+                                    }
+
+                                    val excited = if (arousals[0].title == 0) {
+
+                                        "Excitedness Level : ${
+                                            String.format(
+                                                "(%.1f%%) ",
+                                                (1 - arousals[0].confidence) * 100.0f
+                                            )
+                                        }"
+                                    } else {
+                                        "Excitedness Level : ${
+                                            String.format(
+                                                "(%.1f%%) ",
+                                                arousals[0].confidence * 100.0f
+                                            )
+                                        }"
+                                    }
+                                    cameraBinding.tvAttention.text = attention
+                                    cameraBinding.tvExcited.text = excited
+
+                                    when (emotion) {
+                                        EmotionConstants.ANGRY -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_angry)
+                                        }
+                                        EmotionConstants.CONTEMPT -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_contempt)
+                                        }
+                                        EmotionConstants.DISGUSTED -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_disgust)
+                                        }
+                                        EmotionConstants.FEARFUL -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_fearful)
+                                        }
+                                        EmotionConstants.HAPPY -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_happy)
+                                        }
+                                        EmotionConstants.NEUTRAL -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_neutral)
+                                        }
+                                        EmotionConstants.SAD -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_sad)
+                                        }
+                                        EmotionConstants.SURPRISED -> {
+                                            cameraBinding.ivEmotionEmoji.load(R.drawable.ic_emoji_surprised)
+                                        }
+                                    }
+
+                                }
+                            } catch (e: Exception) {
+                                Timber.e("Error in Compound Emotion $e")
+                            }
+                        }
                     } catch (e: Exception) {
                         Timber.e("Error in Emotion $e")
                     }
